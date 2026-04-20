2f134a4af0461fdcc90902c6ced414965fbcb9d2
KennyLi6 <kennyli3219@gmail.com>
2026-04-20 16:06:49 -0700

    Revert "Merge pull request #2 from KennyLi6/lesson-2"
    This reverts commit 8215ee9fbf8f93ec504eabc4bb0ed3371a059a39, reversing
    changes made to 4c2b2dc32f5a48b7c5190583fd81b3ab0a5ef9b6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
 
 function App() {
 
+  const todoList = [
+    {id: 1, title: "read lesson"},
+    {id: 2, title: "view resources"},
+    {id: 3, title: "code app"}
+  ]
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <TodoList />
+      <ul>
+        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+      </ul>
     </div>
   )
 }

de408f76bc359e36004096fe79f2fbd9cfa741d0
KennyLi6 <kennyli3219@gmail.com>
2026-04-20 18:12:42 -0700

    added useState functionality in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
+import { useState } from 'react'
+
+const todos = [
+  {id: 1, title: "read lesson"},
+  {id: 2, title: "view resources"},
+  {id: 3, title: "code app"}
+]
 
 function App() {
 
-
+  const [todoList, setTodoList] = useState(todos)
+  
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   )
 }

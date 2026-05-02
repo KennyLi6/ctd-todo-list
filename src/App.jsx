3e1ffa6ee938c820e46fd205a05ac4c5c3fbdc7e
KennyLi6 <kennyli3219@gmail.com>
2026-05-01 18:54:09 -0700

    create complete todo function

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,31 @@ function App() {
   function addTodo(todoTitle) {
     const newTodo = {
       id: Date.now(), 
-      title: todoTitle
+      title: todoTitle,
+      isCompeleted: false
     }
 
     setTodoList(previous => [newTodo, ...previous])
+  }
+
+  function completeTodo(id) {
+    let todo = todoList.map(todo => {
+      if (todo.id == id) {
+        return todo;
+      }
+    })
+    console.log(todo)
+    if (todo !== undefined) {
+      todo = {...todo, isCompeleted: true}
+      setTodoList(previous => [todo, ...previous])
+    }
   }
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
     </div>
   )
 }

21923148b7e13f816e9ec92cb517bf41cbdac548
KennyLi6 <kennyli3219@gmail.com>
2026-05-07 16:17:55 -0700

    add update todo functionality

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
+import { isValidTodoTitle } from "../../utils/todoValidation";
 
-function TodoListItem({ todo, onCompleteTodo }) {
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
     const [isEditing, setIsEditing] = useState(false);
     const [workingTitle, setWorkingTitle] = useState(todo.title);
 
@@ -13,10 +14,19 @@ function TodoListItem({ todo, onCompleteTodo }) {
     function handleEdit(event) {
         setWorkingTitle(event.target.value)
     }
+
+    function handleUpdate(event) {
+        if (!isEditing) return;
+        event.preventDefault();
+        onUpdateTodo({...todo, title: workingTitle})
+        setIsEditing(false);
+    }
     
     return (
         <li>
-            <form>
+            <form
+                onSubmit={handleUpdate}
+            >
                 {isEditing ? (
                     <>
                         <TextInputWithLabel 
@@ -28,6 +38,13 @@ function TodoListItem({ todo, onCompleteTodo }) {
                             onClick={handleCancel}
                         >
                         Cancel
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleUpdate}
+                            disabled={!isValidTodoTitle(workingTitle)}
+                        >
+                        Update
                         </button>
                     </>
                 ) : (

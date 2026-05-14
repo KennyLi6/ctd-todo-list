c406e6130244bcb1522a47680d83a221df3c71a8
KennyLi6 <kennyli3219@gmail.com>
2026-05-14 15:35:23 -0700

    Add Log In feature to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+import Logon from './features/Logon'
 import TodosPage from './features/Todos/TodoPage'
 import Header from './shared/Header'
 
 function App() {
-  
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+
   return (
     <>
-      <Header />
-      <TodosPage />
+      <Header 
+        token={token} 
+        onSetToken={setToken} 
+        onSetEmail={setEmail}
+      />
+      {token ? (
+        <TodosPage token={token}/>
+      ) : (
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />
+      )}
     </>
-  )
+  );
 }
 
 export default App

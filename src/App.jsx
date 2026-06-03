862570d74ef7016fae222561c6d9969f4f6fb161
KennyLi6 <kennyli3219@gmail.com>
2026-06-03 16:14:48 -0700

    Replace auth use with Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
 import Logon from './features/Logon'
 import TodosPage from './features/Todos/TodosPage'
 import Header from './shared/Header'
-import { useAuth } from './contexts/AuthContext';
 
 function App() {
-  const { isAuthenticated } = useAuth();
 
   return (
     <>
       <Header />
-      {isAuthenticated ? (
-        <TodosPage/>
-      ) : (
-        <Logon/>
-      )}
+        <Routes>
+
+        </Routes>
     </>
   );
 }
